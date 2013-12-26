197a995b835dc7dd0f4bb1615c8f3e9c20eced3b
thehacka1 <kalakotig@gmail.com>
2013-12-26 10:32:45 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
+add_lunch_combo cm_marvel-debug
 add_lunch_combo cm_marvel-userdebug
 add_lunch_combo cm_marvel-eng
