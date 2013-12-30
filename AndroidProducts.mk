1d101ba975044bae5a173cd3c636ec38e982036e
thehacka1 <kalakotig@gmail.com>
2013-12-30 22:58:33 +0000

    Add missing separators

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,6 +18,6 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_marvel.mk
-    $(LOCAL_DIR)/cm.mk
+    $(LOCAL_DIR)/device_marvel.mk \
+    $(LOCAL_DIR)/cm.mk \
     $(LOCAL_DIR)/full_marvel.mk
